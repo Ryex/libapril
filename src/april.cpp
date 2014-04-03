cb3b48ecdcc32ec0569843b1672ac499612e6087
borisblizzard <borisblizzard@ff5e82f7-e874-4a17-afd9-df9bbb55e678>
2014-04-03 11:00:03 +0000

    git-svn-id: svn://svn.code.sf.net/p/libapril/code/trunk@1501 ff5e82f7-e874-4a17-afd9-df9bbb55e678

--- src/april.cpp
+++ src/april.cpp
@@ -9,6 +9,8 @@
 /// This program is free software; you can redistribute it and/or modify it under
 /// the terms of the BSD license: http://www.opensource.org/licenses/bsd-license.php
 
+#pragma message("WARNING! This version of the library is deprecated! Please use the one from: http://libapril.googlecode.com")
+
 #ifdef _OPENKODE
 #include <KD/kd.h>
 #endif
